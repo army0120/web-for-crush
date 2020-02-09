b8201e35e0f14e43b54c5f907f7d50e2f4bff682
army0120 <57707315+army0120@users.noreply.github.com>
2020-02-09 19:48:00 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -122,7 +122,7 @@ $('#yes').click(function() {
                 confirmButtonText: 'Okiiiii lun <3',
                 background: '#fff url("img/iput-bg.jpg")',
                 title: 'Tớ biết mà ^^ Yêu cậu 300.000',
-                text: "Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo",
+                text: "Tối nay tôi đón ông ra net nha :3 Còn giờ thì chờ gì nữa mà ko chuyển khoản cho tôi đi nàoooooo",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
                     window.location = 'http://fb.com';
